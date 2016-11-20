db9dc40a8e692695e1c3867ee327aba077a7ba3d
nms377 <nms377@nyu.edu>
2016-11-19 14:03:45 -1000

    Working on creating the content to make the function pass.

--- linkedList.js
+++ linkedList.js
@@ -5,5 +5,46 @@
  */
 function linkedListGenerator(){
 
+	var head = null;	// null has no value...yet!
+
+//Retrieves the value of the first node in a list
+	function getHead(){
+
+		return head;	//return null
+
+	}
+
+//Retrieves the value of the last node in a list
+	function getTail(){
+
+		return head;
+
+	}
+
+	function add(){
+
+	}
+
+	function remove(){
+
+	}
+
+	function get(){
+
+	}
+
+	function insert(){
+
+	}
+
+	return{
+		getHead: getHead,
+		getTail: getTail,
+		add: add,
+		remove: remove,
+		get: get,
+		insert: insert
+
+	};
 
 }
