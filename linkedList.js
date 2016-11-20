a88b954ec8881ef8a88f23815c7f8f5350759b6e
nms377 <nms377@nyu.edu>
2016-11-19 14:15:22 -1000

    Adding notes for clarity.

--- linkedList.js
+++ linkedList.js
@@ -8,20 +8,32 @@ function linkedListGenerator(){
 	var head = null;	// null has no value...yet!
 
 //Retrieves the value of the first node in a list
+
+	// [ head ] -> [ node 1 ] -> [ node 2 ] -> [ tail ]
+	//		^ first node in the list
+
 	function getHead(){
 
 		return head;	//return null
 
 	}
 
-//Retrieves the value of the last node in a list
+//Retrieves the value of the first node in a list
 	function getTail(){
 
-		return head;
+		return head;	//This passes, I wonder how this will affect the rest of my code
 
 	}
 
+//Creates a new node object after appending new node to the list
 	function add(){
+		var newNode = {
+			value: null,	//put null because I can't remember what goes here
+			next: null		//put null because we don't know where we're appending the node
+		};
+
+	//	[ head ] -> [ index 1 ] -> [ index 2 ] -> [ tail ]
+	//	[ newNode ] want to append to the list somewhere
 
 	}
 
@@ -48,3 +60,11 @@ function linkedListGenerator(){
 	};
 
 }
+
+var ll = linkedListGenerator();
+
+ll.getHead();
+console.log(ll.getHead(0));	//return null
+
+ll.getTail();
+console.log(ll.getTail(0));	//return null
